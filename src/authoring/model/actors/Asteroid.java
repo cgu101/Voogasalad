06ce6779d8eba6d4f88e7dcfd6a5235cd08be1ac
Christopher Streiffer <cds33@duke.edu>
2015-11-15 18:35:27 -0500

    Adds constructor for actors.

--- src/authoring/model/actors/Asteroid.java
+++ src/authoring/model/actors/Asteroid.java
@@ -1,8 +1,12 @@
 package authoring.model.actors;
 
-import authoring.model.actions.IAction;
 import authoring.model.bundles.Bundle;
+import authoring.model.properties.Property;
 
 public class Asteroid extends Actor {
+
+	public Asteroid(Bundle<Property<?>> myPropertyBundle, String identifier) {
+		super(myPropertyBundle, identifier);
+	}
 
 }
