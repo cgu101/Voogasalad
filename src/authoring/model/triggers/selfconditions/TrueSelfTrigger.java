22760f214380d49d4daf3c06dc50442270939562
Tyler Webner <tlw37@duke.edu>
2015-11-17 18:34:28 -0500

    modified properties

--- src/authoring/model/triggers/selfconditions/TrueSelfTrigger.java
+++ src/authoring/model/triggers/selfconditions/TrueSelfTrigger.java
@@ -1,17 +1,18 @@
-package authoring.model.triggers.externaltriggers;
+package authoring.model.triggers.selfconditions;
 
 import java.util.List;
 
 import authoring.model.actions.IAction;
 import authoring.model.actors.Actor;
 import authoring.model.actors.ActorGroups;
+import authoring.model.triggers.selftriggers.ASelfTrigger;
 import player.InputManager;
 
-public class TrueExternalTrigger extends AExternalTrigger {
-
-	public TrueExternalTrigger() {
+public class TrueSelfTrigger extends ASelfTrigger {
+		
+	public TrueSelfTrigger() {
 	}
-
+		
 	@Override
 	public boolean condition(List<IAction> actions, ActorGroups actorGroup, InputManager inputManager, Actor... actors) {
 		return performActions(actions, actorGroup, actors);
