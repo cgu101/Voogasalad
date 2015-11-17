84d1442c688ae160fc811c62a87b0100ce61a94a
Daniel Song <daniel100333@yahoo.com>
2015-11-16 23:46:54 -0500

    Pannable Map
    Version without zooming
    
    There is a bug with the getMap() method, cannot figure out exactly what
    the issue is. Seems to have something to do with bound width and height.
    
    ToDo: Finish implementing the slider bar + zooming.

--- src/view/element/LevelMap.java
+++ src/view/element/LevelMap.java
@@ -1,15 +1,21 @@
 package view.element;
 
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import view.screen.AbstractScreen;
 
 public class LevelMap extends AbstractElement {
 	private Tab myTab;
-	private Rectangle map;
+	private Map map;
 	private AbstractScreen screen;
+	private Image backgroundImage;
+	private ScrollPane sp;
 
 	public LevelMap(GridPane pane, int i, AbstractScreen screen) {
 		super(pane);
@@ -18,6 +24,7 @@ public class LevelMap extends AbstractElement {
 		myTab.setContent(pane);
 		myTab.setId(Integer.toString(i));
 		this.screen = screen;
+		map = new Map(screen);
 		makePane();
 	}
 
@@ -27,15 +34,38 @@ public class LevelMap extends AbstractElement {
 
 	@Override
 	protected void makePane() {
-		map = new Rectangle();
-		map.setFill(Color.valueOf(myResources.getString("background")));
-		map.widthProperty().bind(screen.getScene().widthProperty());
-		map.heightProperty().bind(screen.getScene().heightProperty());
-		pane.add(map, 0, 0);
+//		Test Narnia map image
+		backgroundImage = new Image("http://www.narniaweb.com/wp-content/uploads/2009/08/NarniaMap.jpg");
+		
+		map.addMapElements(new ImageView(backgroundImage));
+		map.createGroups();
+		map.createScrollPane();
+				
+		//Tests to figure out ScrollPane issue		
+//		sp = new ScrollPane();
+//		StackPane layout = new StackPane();
+//		layout.getChildren().addAll(new ImageView(backgroundImage));
+//		
+//		sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+//		sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+//		sp.setPannable(true);
+//		
+//		sp.prefWidthProperty().bind(screen.getScene().widthProperty());
+//		sp.prefHeightProperty().bind(screen.getScene().heightProperty());
+//		
+//		sp.setContent(layout);
+		
+		pane.add(map.getTheMap(), 0, 0);
 	}
 
-	public Rectangle getMap() {
-		return map;
+	public ScrollPane getMap() {
+		//There is a problem with this method. Nothing shows up on screen
+		//When it returns something. Suspect it has to do with the bound
+		//Width and Height.
+		
+		
+		return null;
+		//return map.getTheMap();
 	}
 
 }
