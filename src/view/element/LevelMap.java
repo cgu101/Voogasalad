7663e62b897cac8eaf894dcf216c05ace744f004
Daniel Song <daniel100333@yahoo.com>
2015-11-17 18:57:28 -0500

    Moved Last Methods from LevelMap to Map

--- src/view/element/LevelMap.java
+++ src/view/element/LevelMap.java
@@ -3,17 +3,11 @@ package view.element;
 import authoring.controller.constructor.LevelConstructor;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import view.screen.AbstractScreen;
 
 public class LevelMap extends Map {
 	private Tab myTab;
-	private AbstractScreen screen;
-	private Image backgroundImage;
 	private ScrollPane sp;
 	private LevelConstructor constructor;
 
@@ -29,22 +23,6 @@ public class LevelMap extends Map {
 
 	public Tab getTab() {
 		return myTab;
-	}
-
-	@Override
-	protected void makePane() {
-		//Test Narnia map image
-		backgroundImage = new Image("http://www.narniaweb.com/wp-content/uploads/2009/08/NarniaMap.jpg");
-		
-		//Test white rectangle
-		Rectangle test = new Rectangle(640, 480);
-		test.setFill(Color.WHITE);
-		
-		//Add any elements you want to appear on the map using this method
-		addMapElements(test);
-		
-		//Create the map after adding elements you want
-		createTheMap();
 	}
 	
 }
--- src/view/element/Map.java
+++ src/view/element/Map.java
@@ -3,8 +3,11 @@ package view.element;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
 import view.screen.AbstractScreen;
 
@@ -12,31 +15,30 @@ public class Map extends AbstractElement {
 	private AbstractScreen screen;
 	private Group contentGroup;
 	private Group zoomGroup;
-	
+
 	private StackPane layout;
 	private ScrollPane mapArea;
 	private MapZoomSlider slider;
 
-	
-	
+
+
 	public Map(GridPane pane, AbstractScreen screen) {
 		super(pane);
 		findResources();
 		this.screen = screen;
-		
+
 		//Use a StackPane so we can layer things on top of one another, like an actor
 		//over a background tile. Note that you can probably use something else if a
 		//StackPane is not appropriate, such as a Canvas.
 		layout = new StackPane();
 		mapArea = new ScrollPane();
-		
-		
+
 	}
-	
+
 	public void addMapElements(Node... elements) {
 		layout.getChildren().addAll(elements);
 	}
-	
+
 	public void createTheMap() {
 		createMapArea();
 		slider = new MapZoomSlider(zoomGroup, Double.valueOf(myResources.getString("sliderwidth")));
@@ -44,39 +46,39 @@ public class Map extends AbstractElement {
 		pane.add(mapArea, 0, 0);
 		pane.add(slider.getTheSlider(), 0, 1);
 	}
-	
+
 	private void createMapArea() {
 		createGroups();
 		createScrollPane();
 	}
-	
+
 	private void createScrollPane() {
 		mapArea = new ScrollPane();
-		
+
 		//Hide the vertical and horizontal scrollbars, make the pane pannable
 		mapArea.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 		mapArea.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 		mapArea.setPannable(true);
-		
+
 		//Bind the preferred size of the scroll area to the size of the scene
 		mapArea.prefWidthProperty().bind(screen.getScene().widthProperty());
 		mapArea.prefHeightProperty().bind(screen.getScene().heightProperty());
 
 		mapArea.setContent(contentGroup);
 	}
-	
+
 	private void createGroups() {
 		//Groups all the nodes that will appear on the map and allows them to be 
 		//collectively rescaled using a Scale.
 		zoomGroup = new Group();
-		
+
 		//Contains the zoomGroup that will then be displayed on the map
 		contentGroup = new Group();
-		
+
 		contentGroup.getChildren().add(zoomGroup);
 		zoomGroup.getChildren().add(layout);
 	}
-	
+
 	public void setMapTransform(Scale scaleTransform) {
 		zoomGroup.getTransforms().clear();
 		zoomGroup.getTransforms().add(scaleTransform);
@@ -84,13 +86,23 @@ public class Map extends AbstractElement {
 
 	@Override
 	protected void makePane() {
-		// TODO Auto-generated method stub
-		
+		//Test Narnia map image
+		Image backgroundImage = new Image("http://www.narniaweb.com/wp-content/uploads/2009/08/NarniaMap.jpg");
+
+		//Test white rectangle
+		Rectangle test = new Rectangle(640, 480);
+		test.setFill(Color.WHITE);
+
+		//Add any elements you want to appear on the map using this method
+		addMapElements(test);
+
+		//Create the map after adding elements you want
+		createTheMap();
 	}
-	
+
 	public GridPane getMap() {
 		//Problem from earlier is still present
-		
+
 		return null;
 		//return theMapElement;
 	}
