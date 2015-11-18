de0bcfcd2849a56983a2d9cf45295d3d48f7d116
Austin Liu <abl17@duke.edu>
2015-11-17 21:05:47 -0500

    Finished Saving utility for CreatorScreen

--- src/authoring/controller/AuthoringController.java
+++ src/authoring/controller/AuthoringController.java
@@ -7,13 +7,19 @@ import authoring.controller.constructor.ConstructorFactory;
 import authoring.controller.constructor.LevelConstructor;
 import authoring.model.game.Game;
 import controller.AController;
+import data.IFileManager;
+import data.XMLManager;
+import exceptions.data.GameFileException;
 import javafx.stage.Stage;
 import view.screen.AbstractScreen;
 import view.screen.CreatorScreen;
 
 public class AuthoringController extends AController {
 
+	IFileManager myXMLManager;
+	
 	public AuthoringController() {
+		myXMLManager = new XMLManager();
 	}
 	
 	public AuthoringController (Stage stage) {
@@ -35,6 +41,10 @@ public class AuthoringController extends AController {
 	@Override
 	public void run() throws Exception {
 		currentScreen.run();
+	}
+	
+	public void saveGame (Game game, String fileLocation) throws GameFileException {
+		myXMLManager.saveGame(game, fileLocation);
 	}
 
 	public Game getGameWithLevels (List<LevelConstructor> levelBuilderList) {
