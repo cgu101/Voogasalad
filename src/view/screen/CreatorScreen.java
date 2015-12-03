d7dfbd2e64ca7a3c886eae29f34ff995cb97c752
Connor Usry <cgu1001@gmail.com>
2015-12-03 01:27:41 -0500

    Sends GameWindow into LevelMap so message is sent on Drag/Drop for Networking.

--- src/view/screen/CreatorScreen.java
+++ src/view/screen/CreatorScreen.java
@@ -122,6 +122,7 @@ public class CreatorScreen extends AbstractScreen {
 	
 	public void setGameWindow(GameWindow g) {
 		this.g = g;
+		w.setGameWindow(g);
 	}
 	
 	public GameWindow getGameWindow() {
