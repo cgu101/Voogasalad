94749ef19d7d6443fbcacca8f6661d74bc95a649
Tyler Webner <tlw37@duke.edu>
2015-12-05 21:12:12 -0500

    refactor triggers

--- src/authoring/model/triggers/selfconditions/DistanceTraveledCheck.java
+++ src/authoring/model/triggers/selfconditions/DistanceTraveledCheck.java
@@ -3,15 +3,15 @@ package authoring.model.triggers.selfconditions;
 import authoring.model.actors.Actor;
 import authoring.model.properties.Property;
 import authoring.model.tree.Parameters;
-import authoring.model.triggers.selftriggers.ASelfTrigger;
+import authoring.model.triggers.selftriggers.AActorSelfTrigger;
 import player.InputManager;
 
-public class DistanceTraveledCheck extends ASelfTrigger {
+public class DistanceTraveledCheck extends AActorSelfTrigger {
 	public DistanceTraveledCheck() {
 	}
 
 	@Override
-	public boolean condition(Parameters parameters, InputManager inputManager, Actor actor) {
+	public boolean condition(Parameters parameters, Actor actor) {
 		Double distanceTraveled = actor.getPropertyValue("distanceTraveled");
 		double maxDistance = 400;
 		return (Double.compare(distanceTraveled, maxDistance) >= 0);
