25f8c0c1a50caf414346578ef21fb045f5d7a8a0
Tyler Webner <tlw37@duke.edu>
2015-12-08 01:53:49 -0500

    added losegame and wingame

--- src/authoring/model/actions/oneActorActions/LoseGame.java
+++ src/authoring/model/actions/oneActorActions/LoseGame.java
@@ -8,18 +8,17 @@ import engine.State;
 import resources.keys.PropertyKey;
 import resources.keys.PropertyKeyResource;
 
-public class NextLevel extends AOneActorAction {
+public class LoseGame extends AOneActorAction {
 	/**
 	 * Generated serial version ID
 	 */
-	private static final long serialVersionUID = 4542051448186712167L;
-
+	private static final long serialVersionUID = -5800650791757585890L;
 	private String levelKey = PropertyKeyResource.getKey(PropertyKey.LEVEL_ID_KEY);
 	private String nextLevelKey = PropertyKeyResource.getKey(PropertyKey.NEXT_LEVEL_KEY);
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void run(Parameters parameters, State state, Actor a) {
-		state.getPropertyBundle().add(new Property<String>(levelKey, nextLevelKey));
+		//TODO
 	}
 }
--- src/authoring/model/actions/oneActorActions/WinGame.java
+++ src/authoring/model/actions/oneActorActions/WinGame.java
@@ -8,18 +8,17 @@ import engine.State;
 import resources.keys.PropertyKey;
 import resources.keys.PropertyKeyResource;
 
-public class NextLevel extends AOneActorAction {
+public class WinGame extends AOneActorAction {
 	/**
 	 * Generated serial version ID
 	 */
-	private static final long serialVersionUID = 4542051448186712167L;
-
+	private static final long serialVersionUID = 6822754399215993372L;
 	private String levelKey = PropertyKeyResource.getKey(PropertyKey.LEVEL_ID_KEY);
 	private String nextLevelKey = PropertyKeyResource.getKey(PropertyKey.NEXT_LEVEL_KEY);
 
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void run(Parameters parameters, State state, Actor a) {
-		state.getPropertyBundle().add(new Property<String>(levelKey, nextLevelKey));
+		//TODO
 	}
 }
