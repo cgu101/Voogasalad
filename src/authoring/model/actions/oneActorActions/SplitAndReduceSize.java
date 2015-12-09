ebf32c49d4643396331abae3fae65b77554ee3d0
Tyler Webner <tlw37@duke.edu>
2015-12-09 08:22:16 -0500

    games

--- src/authoring/model/actions/oneActorActions/SplitAndReduceSize.java
+++ src/authoring/model/actions/oneActorActions/SplitAndReduceSize.java
@@ -5,6 +5,7 @@ import authoring.model.ActionTriggerHelper;
 import authoring.model.actions.AOneActorAction;
 import authoring.model.actors.Actor;
 import authoring.model.actors.ActorGroups;
+import authoring.model.bundles.Bundle;
 import authoring.model.tree.Parameters;
 import engine.State;
 import player.InputManager;
@@ -25,20 +26,32 @@ public class SplitAndReduceSize extends AOneActorAction {
 		Double size = actor.getPropertyValue(ActorProperties.SIZE.getKey());
 		Double health = actor.getPropertyValue(ActorProperties.HEALTH.getKey());
 		Double angle = actor.getPropertyValue(ActorProperties.ANGLE.getKey());
+		Double width = actor.getPropertyValue(ActorProperties.WIDTH.getKey());
+		Double height = actor.getPropertyValue(ActorProperties.HEIGHT.getKey());
+		
 
-		Actor futureActor1 = new Actor(actor.getProperties(), ActionTriggerHelper.createActorID());
-		Actor futureActor2 = new Actor(actor.getProperties(), ActionTriggerHelper.createActorID());
+		
+		Actor futureActor1 = new Actor(new Bundle(actor.getProperties()), ActionTriggerHelper.createActorID() + "1");
+		Actor futureActor2 = new Actor(new Bundle(actor.getProperties()), ActionTriggerHelper.createActorID() + "2");
 		futureActor1.setProperty(ActorProperties.SIZE.getKey(), size / 2);
 		futureActor2.setProperty(ActorProperties.SIZE.getKey(), size / 2);
 		futureActor1.setProperty(ActorProperties.HEALTH.getKey(), health / 2);
 		futureActor2.setProperty(ActorProperties.HEALTH.getKey(), health / 2);
 		futureActor1.setProperty(ActorProperties.ANGLE.getKey(), angle + 45);
 		futureActor2.setProperty(ActorProperties.ANGLE.getKey(), angle - 45);
+		futureActor1.setProperty(ActorProperties.WIDTH.getKey(), width / 2);
+		futureActor2.setProperty(ActorProperties.WIDTH.getKey(), width / 2);
+		futureActor1.setProperty(ActorProperties.HEIGHT.getKey(), height / 2);
+		futureActor2.setProperty(ActorProperties.HEIGHT.getKey(), height / 2);
+		
 		ActionTriggerHelper.moveToAvoidCollision(futureActor1, futureActor2);
+		
+
 
 		ActorGroups actorGroups = state.getActorMap();
 		actorGroups.killActor(actor);
 		actorGroups.createActor(futureActor1);
 		actorGroups.createActor(futureActor2);
 	}
+	
 }
